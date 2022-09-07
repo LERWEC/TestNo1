09fd1a00ce2abb69b38606f5ba9a3da896e5729e
LERWEC <106245467+LERWEC@users.noreply.github.com>
2022-09-07 23:23:38 +0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,14 +3,18 @@ let lenLi = undefined;
 let limit = 10;
 let pgEl = `_page=${pageNum}`
 let com = '<h4>Коментарии</h4><p>Саша:Ну такое<br>Foirt:Tye emose tritedorio cumpacto espansio<br>Саша:Ну а я то тебя прекрасно понимаю</p> '
+let listDelite = []
+let arrAddElem = []
+let srchVersion = true
+let vl = ''
 
 function respFch(resp) {
-    let reLe = resp.length;
-    if(reLe == 0){
+    let respLength = resp.length;
+    if(respLength == 0){
         pageNum--
         return false
-    }else if(reLe < 10){
-        lenLi = reLe - 1
+    }else if(respLength < 10){
+        lenLi = respLength - 1
         for (let i = 9; i > lenLi; i--){
             document.getElementById(`el${i}`).classList.toggle('close')
         }
@@ -20,10 +24,22 @@ function respFch(resp) {
         }
         lenLi = undefined;
     }
-    for (let i = 0; i < reLe; i++) { document.getElementById(`el${i}`).innerHTML = `<h2>${resp[i].id}.${resp[i].title}</h2><h3>${resp[i].body}</h3><button onclick="openEl(${resp[i].id})">Open</button><button onclick="removeEl(event)">Del</button>` }
 
-    console.log(pageNum)
-    console.log(pgEl.length)
+    for (let i = 0; i < resp.length; i++){
+        if(listDelite.includes(`${resp[i].id}`)){
+            resp.splice(i, 1)
+            console.log(resp.length)
+            i--
+        }
+    }
+
+    if(resp.length < respLength){
+        respFch(resp)
+    }else{
+        for (let i = 0; i < respLength; i++) { 
+            document.getElementById(`el${i}`).innerHTML = `<h2>${resp[i].id}</h2><h2>${resp[i].title}</h2><h3>${resp[i].body}</h3><button onclick="openEl(${resp[i].id})">Open</button><button onclick="removeEl(event)">Del</button>`
+        }
+    }
 }
 
 fetch(`https://jsonplaceholder.typicode.com/posts?${pgEl}&_limit=10`).then(response => response.json()).then(response => respFch(response));
@@ -33,7 +49,7 @@ fetch(`https://jsonplaceholder.typicode.com/posts?${pgEl}&_limit=10`).then(respo
 function nextList() {
     pageNum++;
     if (pgEl.length > 10){
-        serchEl()
+        serchEl(false)
     }else{
         pgEl = `_page=${pageNum}`
         fetch(`https://jsonplaceholder.typicode.com/posts?${pgEl}&_limit=10`).then(response => response.json()).then(response => respFch(response));
@@ -44,46 +60,110 @@ function backList() {
     if (pageNum > 1) {
         pageNum--
         if (pgEl.length > 10) {
-            serchEl()
+            serchEl(false)
         } else {
+            pgEl = `_page=${pageNum}`
             fetch(`https://jsonplaceholder.typicode.com/posts?${pgEl}&_limit=10`).then(response => response.json()).then(response => respFch(response));
         }  
       };
 };
 
-function serchEl() {
-    let form = document.getElementById('serch-form');
-    let vl = form.text.value
-    pgEl = `q=${vl}&_page=${pageNum}`
+function serchEl(i) {
+    let form = document.getElementById('srch');
+    if(i&&form.value == ''){
+        return false
+    }
+    if(i&&form.value!==''){
+        vl = form.value
+        srchVersion = true
+        form.value=''
+    }
 
+    if(pgEl.length < 10){
+        document.getElementById('srch-bck').classList.toggle('open')
+        document.getElementById('btn-add').classList.toggle('srchBack')
+    }
+    if(srchVersion){
+        srchVersion = false
+        pageNum = 1
+    }
+    pgEl = `q=${vl}&_page=${pageNum}`
+    fetch(`https://jsonplaceholder.typicode.com/posts?${pgEl}&_limit=10`).then(response => response.json()).then(response => respFch(response));
+
+    return false
+}
+
+function srchBck(){
+    document.getElementById('srch-bck').classList.toggle('open')
+    document.getElementById('btn-add').classList.toggle('srchBack')
+
+    srchVersion = true
+    pageNum = 1
+    
+    pgEl = `_page=${pageNum}`
     fetch(`https://jsonplaceholder.typicode.com/posts?${pgEl}&_limit=10`).then(response => response.json()).then(response => respFch(response));
 
     return false
 }
 
 function removeEl(e) {
-    let el = e.currentTarget.parentElement
-    el.classList.toggle('close')
-
+    let parentElem = e.currentTarget.parentElement
+    let firstChildElem = parentElem.firstElementChild
+    listDelite.push(firstChildElem.innerHTML)
+    fetch(`https://jsonplaceholder.typicode.com/posts?${pgEl}&_limit=10`).then(response => response.json()).then(response => respFch(response));
+    console.log(listDelite)
 }
 
 function openEl(elId){
     let el = document.getElementById(`op-el`)
     document.getElementById(`btn`).classList.toggle('close')
     document.getElementById(`elem`).classList.toggle('close')
+    document.getElementById(`serch-form`).classList.toggle('srchBack')
+
     el.classList.toggle('open')
     fetch(`https://jsonplaceholder.typicode.com/posts?id=${elId}`).then(response => response.json()).then(response =>{
-        console.log(response)
-        el.innerHTML = `<h2>${response[0].id}.${response[0].title}</h2><h3>${response[0].body}${com}<label for="com">Ост. ком.</label><input type="text" id="srch" name="com"><input type="button" onclick="alert('Нужно авторизоваться!')" value="отпр."></h3><button onclick="bckList()">Bck</button>`
+        el.innerHTML = `<h2>${response[0].id}.${response[0].title}</h2><h3>${response[0].body}${com}<label for="com">Ост. ком.</label><input type="text" id="srch" name="com"><input type="button" onclick="alert('Нужно авторизоваться!')" value="отпр."></h3><button onclick="bckList(event)">Bck</button>`
     });
 }
 
-function bckList() {
+function bckList(e) {
     fetch(`https://jsonplaceholder.typicode.com/posts?${pgEl}&_limit=10`).then(response => response.json()).then(response => respFch(response));
     document.getElementById(`btn`).classList.toggle('close')
     document.getElementById(`elem`).classList.toggle('close')
-    document.getElementById(`op-el`).classList.toggle('open')
+    document.getElementById(`serch-form`).classList.toggle('srchBack')
+
+    let elemParent = e.currentTarget.parentElement
+    elemParent.classList.toggle('open')
 }
 
+function addEl(){
+    document.getElementById(`btn`).classList.toggle('close')
+    document.getElementById(`elem`).classList.toggle('close')
+    document.getElementById(`serch-form`).classList.toggle('srchBack')
+    
+    let elemAdd = document.getElementById(`add-elem`)
+    elemAdd.classList.toggle('open')
+    elemAdd.innerHTML = '<label for="title">Название</label><br><input type="text" id="add-title" name="title"><br><label for="body">Пост</label><br><textarea name="body" id="add-body" cols="30" rows="10"></textarea><br><button onclick="addElemInList(event)">Add</button><br><button onclick="bckList(event)">Bck</button>' 
+}
 
+function addElemInList(e){
+    let addTitle = document.getElementById('add-title');
+    let vlTitle = addTitle.value
+    let addBody = document.getElementById('add-body');
+    let vlBody = addBody.value
+    if(vlTitle&&vlBody){
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: vlTitle,
+    body: vlBody,
+    userId: 11,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+}).then((response) => response.json()).then((response) => alert(`Добавлен новый пост ${response.id}. ${response.title}`));
+bckList(e)
+    }
+}
 
